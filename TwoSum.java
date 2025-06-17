b09bba577c24d2f8532bc8f25694bd8a8757c5da
Harshitha3580 <harshithareddy437@gmail.com>
2025-06-17 11:49:54 +0530

    Update TwoSum.java

--- TwoSum.java
+++ TwoSum.java
@@ -1,26 +1,20 @@
 import java.util.HashMap;
 public class TwoSum {
     public static int[] twoSum(int[] nums, int target) {
-        HashMap<Integer, Integer> map = new HashMap<>(); // Step 1
-
-        for (int i = 0; i < nums.length; i++) { // Step 2
-            int complement = target - nums[i]; // Step 3
-
-            if (map.containsKey(complement)) { // Step 4
-                return new int[] { map.get(complement), i }; // Step 5
+        HashMap<Integer, Integer> map = new HashMap<>(); 
+        for (int i = 0; i < nums.length; i++) { 
+            int complement = target - nums[i]; 
+            if (map.containsKey(complement)) { 
+                return new int[] { map.get(complement), i }; 
             }
-
-            map.put(nums[i], i); // Step 6
+            map.put(nums[i], i); 
         }
-
-        throw new IllegalArgumentException("No two sum solution"); // Step 7
+        throw new IllegalArgumentException("No two sum solution"); 
     }
-
     public static void main(String[] args) {
-        int[] nums = {2, 7, 11, 15}; // Step 8
+        int[] nums = {2, 7, 11, 15}; 
         int target = 17;
-        int[] result = twoSum(nums, target); // Step 9
-
-        System.out.println("Indices: " + result[0] + ", " + result[1]); // Step 10
+        int[] result = twoSum(nums, target); 
+        System.out.println("Indices: " + result[0] + ", " + result[1]); 
     }
 }
